e5f9d7db33878ace844dbe949b6c723f0eb7495d
zikwall <andrey.kapitonov.96@gmail.com>
2022-12-17 23:30:20 +0300

    Day II

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['http://localhost:3000', 'cdn1.ozone.ru'],
+    domains: ['http://localhost:3000', 'cdn1.ozone.ru', 'warmhold.ru', 'warmhold.ru:3006'],
   },
   reactStrictMode: true,
 }
