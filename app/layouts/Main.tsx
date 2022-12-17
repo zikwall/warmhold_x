19fd9906dd1202e823a4936d46087466630bbad5
zikwall <andrey.kapitonov.96@gmail.com>
2022-12-17 22:58:41 +0300

    Day II: disable index

--- app/layouts/Main.tsx
+++ app/layouts/Main.tsx
@@ -43,6 +43,7 @@ const Main = ({children, title}: MainProps) => {
                 <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
                 <meta name="theme-color" content="#ffffff" />
+                <meta name="robots" content="noindex"/>
                 <title>{title}</title>
             </Head>
 
